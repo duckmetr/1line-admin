6851c19c430080ce5ac05c461802231702e89ac3
duckmetr <duckmetr@gmail.com>
2023-02-22 21:33:30 +0200

    init

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'konva/lib/shapes/Circle'
+import 'konva/lib/shapes/Line'
+// import 'konva/lib/shapes/Text'
+import './assets/styles/main.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
